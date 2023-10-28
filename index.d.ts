e5035d10ea956aeb5adb393cdd06147f3fbbaf17
KatKmiotek <katarzyna.kmiotek@yahoo.com>
2023-10-28 11:50:41 +0100

    feat: add workflow

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,9 @@
 import { Page } from "@playwright/test";
 
-export function setupCounter(element: HTMLButtonElement): void;
 export function getCognitoToken(): string;
 export function waitForResponse(
   page: Page,
   url: string,
   statusCode: number,
 ): Promise<void>;
+export function printHello(): string;
--- lib/main.ts
+++ lib/main.ts
@@ -17,7 +17,13 @@ export function getCognitoToken(): string {
 
   const credentials = `${username}:${password}`;
 
-  const base64Credentials = btoa(credentials);
+  const textEncoder = new TextEncoder();
+  const data = textEncoder.encode(credentials);
+  const base64Credentials = Buffer.from(data).toString("base64");
 
   return base64Credentials;
+}
+
+export function printHello(name: string): string {
+  return `Hello ${name}!`;
 }
